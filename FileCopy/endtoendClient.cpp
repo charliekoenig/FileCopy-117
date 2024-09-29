ee457084ab37bc324a8c8917a69d1840b3ef740e
Charlie H. Koenig <ckoeni01@vm-hw02.eecs.tufts.edu>
2024-09-29 16:50:57 -0400

    client sends filenames to server (losing data)

---
+++ FileCopy/endtoendClient.cpp
@@ -0,0 +1,129 @@
+#include "c150dgmsocket.h"
+#include "c150debug.h"
+#include "c150grading.h"
+#include <fstream>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <cstring>                
+#include <cerrno>
+#include <cstring>             
+#include <iostream>             
+
+using namespace std;
+using namespace C150NETWORK;
+
+const int SERVER_ARG = 1;
+const int SRC_DIR    = 2;
+
+void checkDirectory(char *dirname);
+void checkAndPrintMessage(ssize_t readlen, char *msg, ssize_t bufferlen);
+
+int 
+main(int argc, char *argv[]) {
+
+    GRADEME(argc, argv);
+
+    struct dirent *sourceFile;
+    DIR *SRC;
+
+    if (argc != 3) {
+        cerr << "Correct syntax is " << argv[0] << " <SERVER> <SRC DIR>" << endl;
+        exit(1);
+    }
+
+    checkDirectory(argv[SRC_DIR]);
+    SRC = opendir(argv[SRC_DIR]);
+    if (SRC == NULL) {
+        fprintf(stderr,"Error opening source directory %s\n", argv[2]);     
+        exit(8);
+    }
+
+    char *fileName;
+    ssize_t readLen;
+    char incomingMessage[512];
+    while ((sourceFile = readdir(SRC)) != NULL) {
+        fileName = sourceFile -> d_name;
+        // skip the . and .. names
+        if ((strcmp(fileName, ".") == 0) || (strcmp(fileName, "..")  == 0 )) 
+            continue;          // never copy . or ..
+
+        try { 
+            C150DgmSocket *sock = new C150DgmSocket();
+            sock -> setServerName(argv[SERVER_ARG]);
+
+            sock -> write(fileName, strlen(fileName)+1);
+            
+            readLen = sock -> read(incomingMessage, sizeof(incomingMessage));
+            checkAndPrintMessage(readLen, incomingMessage, sizeof(incomingMessage));
+
+        } catch (C150NetworkException& e) {
+            // In case we're logging to a file, write to the console too
+            cerr << argv[0] << ": caught C150NetworkException: " << e.formattedExplanation() << endl;
+        }
+    }
+
+    closedir(SRC);
+    return 0;
+}
+
+
+void
+checkDirectory(char *dirname) {
+    struct stat statbuf;  
+    if (lstat(dirname, &statbuf) != 0) {
+        fprintf(stderr,"Error stating supplied source directory %s\n", dirname);
+        exit(8);
+    }
+
+    if (!S_ISDIR(statbuf.st_mode)) {
+        fprintf(stderr,"File %s exists but is not a directory\n", dirname);
+        exit(8);
+    }
+}
+
+void
+checkAndPrintMessage(ssize_t readlen, char *msg, ssize_t bufferlen) {
+    // 
+    // Except in case of timeouts, we're not expecting
+    // a zero length read
+    //
+    if (readlen == 0) {
+        throw C150NetworkException("Unexpected zero length read in client");
+    }
+
+    // DEFENSIVE PROGRAMMING: we aren't even trying to read this much
+    // We're just being extra careful to check this
+    if (readlen > (int)(bufferlen)) {
+        throw C150NetworkException("Unexpected over length read in client");
+    }
+
+    //
+    // Make sure server followed the rules and
+    // sent a null-terminated string (well, we could
+    // check that it's all legal characters, but 
+    // at least we look for the null)
+    //
+    if(msg[readlen-1] != '\0') {
+        throw C150NetworkException("Client received message that was not null terminated");     
+    };
+
+    //
+    // Use a routine provided in c150utility.cpp to change any control
+    // or non-printing characters to "." (this is just defensive programming:
+    // if the server maliciously or inadvertently sent us junk characters, then we 
+    // won't send them to our terminal -- some 
+    // control characters can do nasty things!)
+    //
+    // Note: cleanString wants a C++ string, not a char*, so we make a temporary one
+    // here. Not super-fast, but this is just a demo program.
+    string s(msg);
+    cleanString(s);
+
+    // Echo the response on the console
+
+    c150debug->printf(C150APPLICATION,"PRINTING RESPONSE: Response received is \"%s\"\n", s.c_str());
+    printf("Response received is \"%s\"\n", s.c_str());
+
+}
