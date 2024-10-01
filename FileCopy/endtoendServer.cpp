97a0d0e84072ae5ebcbf1c23e29a056809709796
Charlie H. Koenig <ckoeni01@vm-hw08.eecs.tufts.edu>
2024-10-01 16:01:11 -0400

    Comments for nastyfileops.cpp and endtoendServer.cpp

--- FileCopy/endtoendServer.cpp
+++ FileCopy/endtoendServer.cpp
@@ -22,13 +22,14 @@ main(int argc, char *argv[]) {
     GRADEME(argc, argv);
 
     if (argc != 4) {
-        cerr << "Correct syntax is " << argv[0] << " <networknastiness> <filenastiness> <targetdir>" << endl;
+        cerr << "Correct syntax is " << argv[0] 
+             << " <networknastiness> <filenastiness> <targetdir>" << endl;
         exit(1);
     }
 
 
     ssize_t readLen;
-    char incomingMessage[512];
+    char incoming[512];
     int nastiness = atoi(argv[NETWORK_NAST_ARG]);
 
     int i = 0;
@@ -36,26 +37,27 @@ main(int argc, char *argv[]) {
         C150DgmSocket *sock = new C150NastyDgmSocket(nastiness);
 
         while(1) {
-            readLen = sock -> read(incomingMessage, sizeof(incomingMessage));
+            readLen = sock -> read(incoming, sizeof(incoming));
             if (readLen == 0) {
-                cerr << "Read zero message on " << i << " iteration. Trying again" << endl;
+                cerr << "Read zero message on " << i 
+                     << " iteration. Trying again" << endl;
                 continue;
             }
 
-            incomingMessage[readLen] = '\0';  // make sure null terminated
-            string incoming(incomingMessage); // Convert to C++ string ...it's slightly
-                                              // easier to work with, and cleanString
-                                              // expects it
+            // null terminate response and convert to C++ string
+            incoming[readLen] = '\0';  
+            string incomingString(incoming); 
 
-            // one function to read file (PBR const unsigned char *), return length buffer (DONE)
+            // read file given from client and set fileContent to array of file characters 
             unsigned char *fileContent;
-            ssize_t bytesRead = readFile(argv[TARGET_DIR], incoming, nastiness, &fileContent);
+            ssize_t bytesRead = readFile(argv[TARGET_DIR], incomingString, nastiness, &fileContent);
             // TODO: Check for -1?
 
-            // one function to create hash, calls SHA1 on content (char *), strenlen(above), output buff
-                    // Create output buffer in main
+            // Output buffer populated with SHA1 hash
             unsigned char obuff[20];
             SHA1((const unsigned  char *)fileContent, bytesRead, obuff);
+            
+            // Temporary: Print hash to terminal
             for (int j = 0; j < 20; j++) {
                 printf("%02x", (unsigned int) obuff[j]);
             }
@@ -66,11 +68,12 @@ main(int argc, char *argv[]) {
             string finalResponse((char *)obuff, 20);
             sock -> write((const char *)obuff, finalResponse.length()+1);
             
-            // stay in same iteration of loop waiting for SUCCESS/FAILURE from client
+            // TODO: Read SUCCESS/FAILURE from client
                 // sock -> read() (retransmisison or not)
                 // print filename + status
                 // send acknoledgement to client
-
+            // Should be engineered in a way that allows us to continue loop, no guarantee the next packet is the confirmation
+                // Each loop should handle one read which is capable of handling any type of packet 
             i++;
         }
             
--- FileCopy/nastyfileops.cpp
+++ FileCopy/nastyfileops.cpp
@@ -3,28 +3,51 @@
 using namespace std;
 using namespace C150NETWORK;
 
+/**********************************************************
+ * Function: makeFileName
+ 
+ * Parameters: 
+    * string dir   -> Directory name containing the file
+    * string fname -> Name of the file within the directory
+
+ * Return: A string representing a relative file path
+***********************************************************/
 string
 makeFileName(string dir, string fname) {
     stringstream ss;
-
     ss << dir;
+
     // make sure dir name ends in /
     if (dir.substr(dir.length()-1,1) != "/")
         ss << '/';
-    ss << fname;     // append file name to dir
-    return ss.str();  // return dir/name
-  
+
+    // append file name to dir
+    ss << fname;    
+
+    // return dir/name
+    return ss.str(); 
 }
 
+/**********************************************************
+ * Function: isFile
+
+ * Parameters: 
+    * string fname -> Relative path of a file
+
+ * Return: A boolean (true if fname is a file, false if not)
+***********************************************************/
 bool
 isFile(string fname) {
     const char *filename = fname.c_str();
     struct stat statbuf;  
+
+    // confirm fname is a file
     if (lstat(filename, &statbuf) != 0) {
         fprintf(stderr,"isFile: Error stating supplied source file %s\n", filename);
         return false;
     }
 
+    // confirm file is regular
     if (!S_ISREG(statbuf.st_mode)) {
         fprintf(stderr,"isFile: %s exists but is not a regular file\n", filename);
         return false;
@@ -32,6 +55,21 @@ isFile(string fname) {
     return true;
 }
 
+/**********************************************************
+ * Function: readFile
+
+ * Parameters: 
+    * string dir -> Directory containing file
+    * string fname -> File name
+    * int nastiness -> File reading nastiness level
+    * unsigned char **obuff -> Pointer to output buffer to hold file contents
+
+ * Return: Length of file as ssize_t, -1 if failure
+
+ * Notes: 
+    * Allocates memory for *obuff that must be freed by caller
+    * Exits program on unsuccessful file operations
+***********************************************************/
 ssize_t
 readFile(string dir, string fname, int nastiness, unsigned char **obuff) {
     struct stat statbuff;
@@ -39,46 +77,54 @@ readFile(string dir, string fname, int nastiness, unsigned char **obuff) {
     size_t length;
     size_t sourceSize;
 
+    // combines directory and file name for full relative file path
     string fullFilePath = makeFileName(dir, fname);
 
+    // checks that the name given is a file
     if (!isFile(fullFilePath)) {
         cerr << "Input file " << fullFilePath << " is a directory or other non-regular file. Skipping" << endl;
         return -1; 
     }
 
     try {
-
         if (lstat(fullFilePath.c_str(), &statbuff) != 0) {
             fprintf(stderr,"copyFile: Error stating supplied source file %s\n", fullFilePath.c_str());
             exit(20);
         }
 
+        // allocate space for *obuff to hold file contents
         sourceSize = statbuff.st_size;
         *obuff = (unsigned char *)malloc(sourceSize);
         
+        // create nasty file based on nastiness given
         NASTYFILE inputFile(nastiness); 
 
+        // open the file, exit on failure
         fopenretval = inputFile.fopen(fullFilePath.c_str(), "rb");  
         if (fopenretval == NULL) {
             cerr << "Error opening input file " << fullFilePath << " errno=" << strerror(errno) << endl;
             exit(12);
         }
 
+        // read entire file, exit if bytes read is not equal to file length
         length = inputFile.fread(*obuff, 1, sourceSize);
         if (length != sourceSize) {
             cerr << "Error reading file " << fullFilePath << " errno=" << strerror(errno) << endl;
             exit(16);
         }
 
+        // close input file, exit on failure
         if (inputFile.fclose() != 0 ) {
             cerr << "Error closing input file " << fullFilePath << " errno=" << strerror(errno) << endl;
             exit(16);
         }
 
-
         return length;
 
     } catch (C150Exception& e) {
+        if (*obuff != null) {
+            free(*obuff);
+        }
         cerr << "endtoendserver:readfile(): Caught C150Exception: " << e.formattedExplanation() << endl;
     } 
 
