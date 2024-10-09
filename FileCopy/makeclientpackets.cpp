7ea2a4f8905f4ad911502fe86aa25818a9dd5937
Charlie H. Koenig <ckoeni01@vm-hw07.eecs.tufts.edu>
2024-10-09 10:36:18 -0400

    Added a preliminary version of makeBytePacket

--- FileCopy/makeclientpackets.cpp
+++ FileCopy/makeclientpackets.cpp
@@ -36,3 +36,32 @@ packet
 makeFilecheckPacket(char *filename, int packetNumber) {
     return makePacket('F', strlen(filename), packetNumber, filename);
 }
+
+packet
+makeBytePacket(int offset, char *filename, char *fileContent, int packetNumber) {
+    int filenameLengthBytes = 1;
+    int offsetBytes         = 3;
+	// these should be passed as parameters, we don’t want strlen failing because of null 
+    int filenameLength      = strlen(filename);
+    int bytesRead           = strlen(fileContent);
+
+    int fileContentIndex = (offsetBytes + filenameLengthBytes + filenameLength);
+    int filenameIndex    = (offsetBytes + filenameLengthBytes) - 1;
+
+    int contentLength = filenameLength + bytesRead + offsetBytes + filenameLengthBytes;
+
+    unsigned char content[contentLength];
+    for (int index = 0; index < contentLength; index++) {
+        if (index >= fileContentIndex) {
+            content[index] = fileContent[index - fileContentIndex];
+        } else if (index >= filenameIndex) {
+            content[index] = filename[index - filenameIndex];
+        } else if (index == 3) {
+            content[index] = (unsigned char)filenameLength;
+        } else {
+            content[index] = (unsigned char)(offset >> ((2 - index) * 8) & 0xFF);
+        }
+    }
+
+    return makePacket('B', contentLength, packetNumber, content);
+}
